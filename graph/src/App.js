64f9ab53eab17cdd6db7cc854449a129c64b1559
penbor <alexandrgoncharov2015@ya.ru>
2021-08-24 22:57:42 +0300

    0.00

---
+++ graph/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'>
+
+      </header>
+      <div className='head table'>
+        <div className='surname'>
+          <p>Surname</p>
+        </div>
+        <div className='days'>
+1
+        </div>
+        <div className='hours-head'>
+2
+        </div>
+        <div className='money'>
+3
+        </div>
+      </div>
+      <div className='main-content'></div>
+    </div>
+  );
+}
+
+export default App;
